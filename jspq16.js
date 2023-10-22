630dfe940553b826a9f2369020d9024019953907
D ROHIT RAJAN <rohitrajan2307@gmail.com>
2023-10-22 19:53:39 +0530

    updae
    upsadic

---
+++ jspq16.js
@@ -0,0 +1,35 @@
+const characters = [
+    {
+      name: "Luke Skywalker",
+      height: 172,
+      mass: 77,
+      eye_color: "blue",
+      gender: "male",
+    },
+    {
+      name: "Darth Vader",
+      height: 202,
+      mass: 136,
+      eye_color: "yellow",
+      gender: "male",
+    },
+    {
+      name: "Leia Organa",
+      height: 150,
+      mass: 49,
+      eye_color: "brown",
+      gender: "female",
+    },
+    {
+      name: "Anakin Skywalker",
+      height: 188,
+      mass: 84,
+      eye_color: "blue",
+      gender: "male",
+    }
+  ];
+  
+  const totalMass = characters.reduce((sum, character) => sum + character.mass, 0);
+  
+  console.log("Total mass of all characters: " + totalMass);
+  
